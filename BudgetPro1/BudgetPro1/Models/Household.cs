3934257bee70a4c3cd98a624da74255942259367
mosesadg <moses.adg@gmail.com>
2015-05-13 09:50:19 -0400

    BudgetPro1 Created
    Connected to Azure

---
+++ BudgetPro1/BudgetPro1/Models/Household.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BudgetPro1.Models
+{
+    public class Household
+    {
+        public int Id { get; set; };
+        public string Name { get; set; };
+        public int MyProperty { get; set; }
+    }
+}
---
+++ BudgetPro1/BudgetPro1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BudgetPro1.Startup))]
+namespace BudgetPro1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
